5b6c8373cfa561d4463c169a40d2c57720485320
danielgogo <p1@t0N20190505>
2020-04-17 10:08:21 +0800

    update

--- platon-recover/src/main/java/xyz/chendai/tools/platonrecover/entity/Recover.java
+++ platon-recover/src/main/java/xyz/chendai/tools/platonrecover/entity/Recover.java
@@ -1,7 +1,11 @@
 package xyz.chendai.tools.platonrecover.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.io.Serializable;
@@ -17,10 +21,12 @@ public class Recover implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     private String address;
+    @Column(precision=50, scale=0)
     private BigInteger balance;
     private Type type;
     private String txHash;
     private Status status;
+    @Column(length=1024)
     private String log;
     private Date createTime;
     private Date updateTime;
