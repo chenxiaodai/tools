8a016f1f5db636b8612ebe9afa0b24ace7231eed
danielgogo <p1@t0N20190505>
2020-04-26 18:01:57 +0800

    update

---
+++ platon-recover/src/test/java/com/platon/tools/platonpress/PlatonRecoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platon.tools.platonpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatonRecoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
